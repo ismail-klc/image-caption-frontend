89588ec31adaecad39d7fe118db4871dd3fcc32b
ismail kılıç <ismail000728@gmail.com>
2021-04-26 16:37:32 +0300

    bootstrap css removed

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,4 @@
-import '../public/css/bootstrap.min.css'
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
 import '../public/css/templatemo-style.css'
 import '../public/fontawesome/css/all.min.css'
 import { wrapper } from '../redux';
